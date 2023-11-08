fd5031e5ba3338dbe01aedda10ce81c5c9654b23
sua918 <ssua918@naver.com>
2023-11-08 16:46:12 +0900

    포인터 연습 재커밋

--- challenge/week10/calculator.c
+++ challenge/week10/calculator.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 
 // 함수 원형 선언
-void summation(double* value1, double* value2, double* result);
-void subtraction(double* value1, double* value2, double* result);
-void multification(double* value1, double* value2, double* result);
-void division(double* value1, double* value2, double* result);
+void Summation(double* value1, double* value2, double* result);
+void Subtraction(double* value1, double* value2, double* result);
+void Multification(double* value1, double* value2, double* result);
+void Division(double* value1, double* value2, double* result);
 
-int main()
-{
-	double a = 2, b = 3;
-	double result;
-	summation(&a, &b, &result);
-	printf("%.0lf + %.0lf = %.2lf \n", a, b, result);
+int main() {
+  double a = 2, b = 3;
+  double result;
+  Summation(&a, &b, &result);
+  printf("%.0lf + %.0lf = %.2lf \n", a, b, result);
 
-	subtraction(&a, &b, &result);
-	printf("%.0lf - %.0lf = %.2lf \n", a, b, result);
+  Subtraction(&a, &b, &result);
+  printf("%.0lf - %.0lf = %.2lf \n", a, b, result);
 
-	multification(&a, &b, &result);
-	printf("%.0lf * %.0lf = %.2lf \n", a, b, result);
+  Multification(&a, &b, &result);
+  printf("%.0lf * %.0lf = %.2lf \n", a, b, result);
 
-	division(&a, &b, &result);
-	printf("%.0lf / %.0lf = %.2lf \n", a, b, result);
+  Division(&a, &b, &result);
+  printf("%.0lf / %.0lf = %.2lf \n", a, b, result);
 
-	return 0;
+  return 0;
 }
 
 // 함수 정의, 포인터를 사용하여 함수 호출만으로 값을 변경한다
-void summation(double* value1, double* value2, double* result) {
-	*result = *value1 + *value2;
+void Summation(double* value1, double* value2, double* result) {
+  *result = *value1 + *value2;
 }
 
-void subtraction(double* value1, double* value2, double* result) {
-	*result = *value1 - *value2;
+void Subtraction(double* value1, double* value2, double* result) {
+  *result = *value1 - *value2;
 }
 
-void multification(double* value1, double* value2, double* result) {
-	*result = *value1 * *value2;
+void Multification(double* value1, double* value2, double* result) {
+  *result = *value1 * *value2;
 }
 
-void division(double* value1, double* value2, double* result) {
-	*result = *value1 / *value2;
+void Division(double* value1, double* value2, double* result) {
+  *result = *value1 / *value2;
 }
